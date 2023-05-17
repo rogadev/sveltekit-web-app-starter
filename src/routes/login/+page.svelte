2343e1e247a9c7eb9ec1ec99ab405d22cc6e5156
Roga <ryanroga@gmail.com>
2023-05-16 20:53:32 -0700

    Updates
    Added steps to get the project working on a new computer in the README. Got the project working on a new computer. ✅

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -2,13 +2,21 @@
 	import { enhance } from '$app/forms';
 </script>
 
-<div class="m-8">
-	<h1>Sign in</h1>
-	<form method="POST" use:enhance>
-		<label for="username">Username</label><br />
-		<input class="form-input" id="username" name="username" /><br />
-		<label for="password">Password</label><br />
-		<input class="form-input" type="password" id="password" name="password" /><br />
-		<input class="btn" type="submit" value="Continue" />
-	</form>
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="card px-8 py-4">
+		<h1 class="mb-4 text-3xl text-center">Sign in</h1>
+		<form method="POST" use:enhance>
+			<label class="label">
+				<span>Username</span>
+				<input class="input" name="username" type="text" placeholder="Input" />
+			</label>
+			<label class="label">
+				<span>Password</span>
+				<input class="input" name="password" type="password" placeholder="Input" />
+			</label>
+			<div class="flex flex-col">
+				<button class="btn variant-filled-primary my-4" type="submit"> Sign in </button>
+			</div>
+		</form>
+	</div>
 </div>
